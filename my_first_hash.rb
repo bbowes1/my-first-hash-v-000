f80a9ba35c2908dd1255ad052852ba433f37608f
Benjamin Bowes <bbowes999@gmail.com>
2018-03-28 10:37:20 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,5 @@
 def my_hash
+  my_data = { "my_name" => "Ben Bowes", "my_age" => "32"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
